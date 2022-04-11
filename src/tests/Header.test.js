0e629f183e5e370ba8c4ee98dcc46994a98baa21
MedBen <77970397+Pooreffects@users.noreply.github.com>
2022-04-11 22:43:58 +0000

    Update Header.test.js

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -3,16 +3,18 @@ import { render, screen, cleanup } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import Header from '../components/Header';
 
-it('Should render Header Links', () => {
-  render(
-    <MemoryRouter>
-      <Header />
-    </MemoryRouter>
-  );
-  expect(screen.getAllByRole('link').length).toBe(3);
-  expect(screen.getByText(/orders/i)).toBeInTheDocument();
-  expect(screen.getByText(/visualize/i)).toBeInTheDocument();
-  expect(screen.getByText(/s4dx/i)).toBeInTheDocument();
+describe('Header', () => {
+  it('Should render Header Links', () => {
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    );
+    expect(screen.getAllByRole('link').length).toBe(3);
+    expect(screen.getByText(/orders/i)).toBeInTheDocument();
+    expect(screen.getByText(/visualize/i)).toBeInTheDocument();
+    expect(screen.getByText(/s4dx/i)).toBeInTheDocument();
+  });
 });
 
 afterEach(cleanup);
